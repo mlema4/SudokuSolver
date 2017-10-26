32de8b333770cc7d8cab2013768740536968b56b
TimYuen <tyuen6@uic.edu>
2017-10-25 22:19:44 -0500

    trying to make frame to run menu

--- Frame.java
+++ Frame.java
@@ -1,4 +1,13 @@
+import javax.swing.JFrame;
 
-public class Frame {
-
+public class Frame extends JFrame{
+	
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Frame");
+		// TODO Auto-generated method stub
+		Menu TopMenu = new Menu();
+		frame.add(TopMenu);
+	}
 }
