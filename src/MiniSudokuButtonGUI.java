c7eea6806916c33806b21f19d7a36b905172ec5c
mlema4 <mlema4@uic.edu>
2017-10-26 16:37:20 -0500

    added CEll and modified menu with mini

--- src/MiniSudokuButtonGUI.java
+++ src/MiniSudokuButtonGUI.java
@@ -7,24 +7,25 @@ public class MiniSudokuButtonGUI extends JFrame
 {
  public JButton[] jbutton;
  public JPanel gridP;
- 
+
  public MiniSudokuButtonGUI()
  {
-  jbutton = new JButton[10];
+  jbutton = new JButton[9];
   gridP = new JPanel(new GridLayout(3,3,5,5));
   gridP.setBorder(new EmptyBorder(5,5,5,5));
-   
-    for (int i = 1; i < 10; i++) {
-     JButton tmp = new JButton("" + i);
-     jbutton[i-1]  = tmp;
-     tmp.setPreferredSize(new Dimension(50, 50));
-     gridP.add(tmp);
+
+    for (int i = 0; i < 9; i++) {
+      Cell tmp = new Cell();
+     jbutton[i]  = tmp.cell;
+     tmp.cell.setPreferredSize(new Dimension(50, 50));
+     gridP.add(tmp.cell);
    }
  }
- 
+
  // public static void main(String[] args)
  // {
  //   MiniSudokuButtonGUI m = new MiniSudokuButtonGUI();
  //    m.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
  // }
 }
+    
